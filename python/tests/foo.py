548a04ef8c556cbb2fc7e9a8d2f4318426a47a9b
Latour <latour.courriel@gmail.com>
2016-04-24 16:57:00 +0200

    [Travis] Trying to understand Travis (part 4).

--- python/tests/foo.py
+++ python/tests/foo.py
@@ -1,5 +1,12 @@
 import os
+print("WORKING DIRECTORY:")
 print(os.getcwd())
+print("LS:")
 print(os.listdir())
+print("LS CHEESESHOP:")
+print(os.listdir('cheeseshop'))
+import sys
+print("PYTHON PATH:")
+print(sys.path)
 import cheeseshop
 print(cheeseshop.cheeses.head())
